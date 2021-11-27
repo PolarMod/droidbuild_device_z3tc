8fa50412fb7533c2d91a87cdfb20f2683e68c614
Andrewerr <p01ar@p01ar.net>
2021-11-28 00:05:11 +0200

    Fixed droidbuild

--- droidbuild.sh
+++ droidbuild.sh
@@ -6,7 +6,7 @@ target_z3tc(){
     TARGET_NEEDS_RESYNC=true
     # Add manifests
     TARGET_LOCAL_MANIFESTS+=$LOCAL_PATH/manifests/z3tc.xml
-    # Set device bitess
+    # Set device bitness
     TARGET_ARCH_BITNESS=32
     # Add directories that would be cleaned after build succeeds
     TARGET_USE_DIRS=(
@@ -17,6 +17,6 @@ target_z3tc(){
     target_build-device
 }
 # Currently dummy droidbuld target is required
-droidbuild(){
+droidbuild_module(){
   
 }
